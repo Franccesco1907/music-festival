549908f5d5aa706202bb7e0e5b379d5f93e72af2
Franccesco1907 <fjaimes@pucp.edu.pe>
2023-10-17 21:56:35 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+// function task(done) {
+//   console.log('My first task');
+  
+//   done();
+// }
+
+// exports.task = task;
+
+const { src, dest, watch, parallel } = require('gulp')
+// CSS
+const sass = require('gulp-sass')(require('sass'))
+const plumber = require('gulp-plumber')
+
+// Imágenes
+const cache = require('gulp-cache')
+const imagemin = require('gulp-imagemin')
+const webp = require('gulp-webp')
+const avif = require('gulp-avif')
+
+function css(done) {
+  src('src/scss/**/*.scss') // Identificar el archivo SASS
+  .pipe( plumber() ) // Si tiene problemas, no detiene el workflow
+  .pipe( sass() ) // Compilarlo
+  .pipe( dest('build/css') ); // Almacenarla en el disco duro
+
+
+  done(); // Callback que avisa a gulp cuando llegamos al final
+}
+
+function images(done) {
+  const options = {
+    optimizationLevel: 3
+  }
+  src('src/img/**/*.{jpg,png}')
+  .pipe( cache( imagemin(options) ) )
+  .pipe( dest('build/img') )
+
+  done();
+}
+
+function versionWebp(done) {
+  const options = {
+    quality: 50
+  }
+  src('src/img/**/*.{jpg,png}')
+  .pipe( webp(options) )
+  .pipe( dest('build/img') )
+  done();
+}
+
+function versionAvif(done) {
+  const options = {
+    quality: 50
+  }
+  src('src/img/**/*.{jpg,png}')
+  .pipe( avif(options) )
+  .pipe( dest('build/img') )
+  done();
+}
+
+function dev(done) {
+  watch('src/scss/**/*.scss', css);
+
+  done();
+}
+
+exports.css = css;
+exports.versionWebp = versionWebp;
+exports.images = images;
+exports.dev = parallel(images, versionWebp, versionAvif, dev);
